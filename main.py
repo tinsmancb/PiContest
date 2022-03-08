155e5234fc3d555db1309973fb92ba6a732f0850
Colin Tinsman <tinsmancb@gmail.com>
2022-03-08 18:48:15 +0000

    Prepared for contest by removing trial implementation.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from mpmath import *
-mp.dps = 100000
+mp.dps = 100
 mp.pretty = True
 
-def rama(n):
-  return 9801/(2*sqrt(2)*sum([factorial(4*k)*(1103+26390*k)/((factorial(k)**4)*(396**(4*k))) for k in range(n)]))
+def compute_pi():
+  return 3
 
 def main():
-  pi_calc = rama(15000)
+  pi_calc = compute_pi()
   diff = (pi_calc - pi)/pi
   print(pi_calc)
-  nprint(-log10(diff), 12)
+  nprint(-log10(abs(diff)), 12)
 
 if __name__ == "__main__":
   main()
