41fa3de01940381e6817e3590bd412db4b5508c2
Colin Tinsman <tinsmancb@gmail.com>
2022-03-11 18:44:03 +0000

    Modify a few comments.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from mpmath import *
 mp.dps = 100 # Increase this to compute more digits.
-# Note: this will result in code that takes longer to run,
-# so make sure your algorithm works before changing this.
+# Note: this will result in code that takes longer to 
+# run, so make sure your algorithm works before 
+# changing this.
 mp.pretty = True
 
 def compute_pi():
   # Implement your pi calculation here.
-  return 3
+  return 3 # The first digit is free.
 
-# DONT MODIFY ANYTHING BELOW THIS LINE!
+# DON'T MODIFY ANYTHING BELOW THIS LINE!
 
 def main():
   pi_calc = compute_pi()
