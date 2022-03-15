8ae918f1d1ce7508b92aa013f26071f1c384f464
Colin Tinsman <tinsmancb@gmail.com>
2022-03-15 18:46:10 +0000

    Only display integer numbers of digits.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
   pi_calc = compute_pi()
   diff = (pi_calc - pi)/pi
   print(pi_calc)
-  nprint(-log10(abs(diff)), 12)
+  nprint(int(-log10(abs(diff))), 12)
 
 if __name__ == "__main__":
   main()
